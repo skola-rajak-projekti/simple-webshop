7fe28333f737e5300c26b084f7345364cbf374d3
skola <skolarajak.programerska@gmail.com>
2019-11-03 18:05:29 +0100

    Remove item from kart button

--- js/kart-page.js
+++ js/kart-page.js
@@ -1,8 +1,22 @@
 var shoopingList;
 var sum = 0;
 
+function removeFilter(id){
+    return shoopingList.filter(function(el){       
+        return el.id != id;
+    });        
+}
+
+function removeItemFromKart(itemId) {  
+  
+    var temp = removeFilter(itemId);
+    window.localStorage.setItem("my-kart", JSON.stringify(temp));
+    location.reload();
+}
+
+
 function printShoppingKart() {
-    root = document.getElementById('table-ordered-items');    
+    root = document.getElementById('table-ordered-items');
     pageContent = ""
 
     if (shoopingList.length == 0) {
@@ -10,31 +24,35 @@ function printShoppingKart() {
         kartHeader = document.getElementById('kart-header');
         kartHeader.style.display = "none";
     } else {
-        pageContent += "<div class='table-responsive'>"+
-        "<table class='table table-striped'>" +
-            "<thead>"+
-                "<tr>"+
-                 "<th scope='col'> # </th>"+
-                    "<th scope='col'> Naziv </th>"+
-                    "<th scope='col'> Kolicina </th>"+
-                    "<th scope='col'> Jedinicna cena </th>"+
-                    "<th scope='col'> Iznos </th>"+
-                    "<th scope='col'> Obrisi </th>"+
-                "</tr>"+
+        pageContent += "<div class='table-responsive '>" +
+            "<table class='table table-striped table-bordered'>" +
+            "<thead>" +
+            "<tr>" +
+            "<th scope='col'> # </th>" +
+            "<th scope='col'> Naziv </th>" +
+            "<th scope='col'> Kolicina </th>" +
+            "<th scope='col'> Jedinicna cena </th>" +
+            "<th scope='col'> Popust </th>" +
+            "<th scope='col'> Iznos </th>" +
+            "<th scope='col'> Obrisi </th>" +
+            "</tr>" +
             "</thead><tbody>";
         shoopingList.forEach(function (item, index) {
             pageContent += "<tr> <td> <b>" + item.id +
                 "</b> </td> <td class='item-blue'>" + item.name +
                 "</td> <td>" + item.amount +
                 "</td> <td>" + item.price +
-                "</td> <td>" + item.price * item.amount +
-                "</td> <td>" + "<button type='button' class='btn btn-link'> </button> <span class='glyphicon glyphicon-trash'> x </span>" +
+                "</td> <td>" + item.discount + "%" +
+                "</td> <td> <strong>" + item.price * item.amount * (100 - item.discount) / 100 + "din</strong>" +
+                "</td> <td>" + "<button type='button' class='btn btn-link' onclick='removeItemFromKart(" + item.id + ")'  <span class='glyphicon glyphicon-trash'> x </span></button>" +
                 "</td></tr>";
-            sum += item.price * item.amount;
+            sum += item.price * item.amount * (100 - item.discount) / 100;
         });
 
-        pageContent += " <tr> <th colspan='40'> Upuni iznos: " + sum + " </th> </tr> </tbody> </table></div>";
 
+        pageContent += "<tr colspan='40'><div class='offset-9'><table class='table  table-bordered'>" +
+            "<tr><td><h5> Ukupuni iznos: " + sum + " din</h5></td></tr></table></div>";
+        pageContent += "</tr></tbody> </table> </div></td></tr>";
         root.innerHTML = pageContent;
 
 
